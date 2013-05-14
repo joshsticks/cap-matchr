f586bb539cbb27d0d6b7426d37a516aed41cd357
Matthew Kesler <mkesler@captechconsulting.com>
2013-05-14 09:32:18 -0400

    chaining

--- web.js
+++ web.js
@@ -1,16 +1,3 @@
-// var express = require("express");
-// var app = express();
-// app.use(express.logger());
-
-// app.get('/', function(request, response) {
-//   response.send('Hello World!');
-// });
-
-// var port = process.env.PORT || 5000;
-// app.listen(port, function() {
-//   console.log("Listening on " + port);
-// });
-
 var express = require('express');
 var app = express.createServer(express.logger());
 
@@ -37,7 +24,15 @@ app.get('/', function(request, response) {
 });
 
 app.get('/api/list', function(request, response) {
-   response.send(200, "success");
+   $.ajax({
+     url:'http://slaughter-spottr.herokuapp.com/api/count',
+      success:function(data) {
+         response.send(200, data);
+      },
+      error: function(jqXHR) {
+         response.send(500, data);
+      }
+   });
 });
 
 var port = process.env.PORT || 5000;
